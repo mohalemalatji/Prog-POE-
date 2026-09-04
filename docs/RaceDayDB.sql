12851781fb46f618349b9199256f4a7189da6046
mohalemalatji <ST10483010@rcconnect.edu.za>
2026-09-04 14:13:10 +0200

    Update RaceDayDB.sql

--- docs/RaceDayDB.sql
+++ docs/RaceDayDB.sql
@@ -48,3 +48,16 @@ CREATE TABLE EVENTCATEGORY (
     CONSTRAINT FK_EventCategory_Category 
         FOREIGN KEY (CategoryID) REFERENCES CATEGORY(CategoryID) ON DELETE CASCADE
 );
+CREATE TABLE ENROLMENT (
+    EnrolmentID VARCHAR(50) PRIMARY KEY,
+    ParticipantID VARCHAR(50) NOT NULL,
+    EventCategoryID VARCHAR(50) NOT NULL,
+    EnrolmentDate DATETIME NOT NULL DEFAULT GETDATE(),
+    Status VARCHAR(50) NOT NULL DEFAULT 'Registered',
+
+    CONSTRAINT FK_Enrolment_Participant 
+        FOREIGN KEY (ParticipantID) REFERENCES [USER](UserID),
+
+    CONSTRAINT FK_Enrolment_EventCategory 
+        FOREIGN KEY (EventCategoryID) REFERENCES EVENTCATEGORY(EventCategoryID)
+);
